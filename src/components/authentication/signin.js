5fdbab8d111c458a64d09b878eb9c35115de98a0
Gauri Gattani <gauri.gattani@massiveinteractive.com>
2016-01-05 10:38:02 +0000

    create components

---
+++ src/components/authentication/signin.js
@@ -0,0 +1,59 @@
+var React = require('react-native');
+
+var {
+	View,
+	Text,
+	StyleSheet,
+	TextInput
+} = React;
+
+var Button = require('../common/button');
+
+module.exports = React.createClass({
+	getInitialState: function() {
+		return {
+			username: '',
+			password: ''
+		};
+	},
+	render: function() {
+		return (
+			<View style={styles.container}>
+				<Text>Sign in</Text>
+
+				<Text style={styles.label}>Username: </Text>
+				<TextInput 
+					value={this.state.username}
+					onChangeText={(text) => this.setState({ username: text})}
+					style={styles.input} />
+				<Text style={styles.label}>Password: </Text>
+				<TextInput secureTextEntry={true} style={styles.input} />
+				<Button text={'Sign In'} onPress={this.onPress}/>
+			</View>	
+		)
+	},
+	onPress: function() {
+
+	}
+});
+
+var styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	input: {
+		padding: 4,
+		height: 40,
+		borderColor: 'gray',
+		borderWidth: 1,
+		borderRadius: 5,
+		margin: 5,
+		width: 200,
+		alignSelf: 'center'
+	},
+	label: {
+		fontSize: 18
+	}
+});
